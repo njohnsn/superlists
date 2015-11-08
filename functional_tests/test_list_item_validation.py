58e9901f9ee1ff6c04d8cc10c67d8020a0c32fd9
Neil Johnson <neil.johnson@erudicon.com>
2015-11-07 21:19:54 -0600

    Adjust new list view to do model validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -15,12 +15,12 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # She tries again with some text for the item, which now works
-        self.browser(self.find_element_by_id('id_new_item').send_keys(
-            'Buy milk\n'))
+        self.browser.find_element_by_id('id_new_item').send_keys(
+            'Buy milk\n')
         self.check_for_row_in_list_table('1: Buy milk')
 
         # Preversely, she now decides to submit a second blan list item
-        self.browser(self.find_element_by_id('id_new_item').send_keys('\n'))
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # She receives a similar warning on the list page
         self.check_for_row_in_list_table('1: Buy milk')
@@ -28,7 +28,7 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # And she can correct it by filling some text in
-        self.browser(self.find_element_by_id('id_new_item').send_keys('Make tea\n'))
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
 
